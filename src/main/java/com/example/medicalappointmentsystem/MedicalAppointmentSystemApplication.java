9a721653c85d8eda48187d58d1064ff9decb0b4b
GUDIVADA PRANEETH KUMAR <163731071+PRANEETHKUMAR30918@users.noreply.github.com>
2025-09-24 07:33:15 +0530

    Update MedicalAppointmentSystemApplication.java

--- src/main/java/com/example/medicalappointmentsystem/MedicalAppointmentSystemApplication.java
+++ src/main/java/com/example/medicalappointmentsystem/MedicalAppointmentSystemApplication.java
@@ -2,12 +2,19 @@ package com.example.medicalappointmentsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MedicalAppointmentSystemApplication {
+public class MedicalAppointmentSystemApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MedicalAppointmentSystemApplication.class, args);
-	}
+    // ✅ Override this method to configure the WAR deployment
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MedicalAppointmentSystemApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalAppointmentSystemApplication.class, args);
+    }
 }
